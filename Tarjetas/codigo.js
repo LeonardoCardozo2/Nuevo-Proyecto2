97e8bc63f5ef5cc38edae19dbabd1f0df8fbd180
Leonardo Cardozo <leonardocardozo153@gmail.com>
2023-04-10 13:43:02 -0300

    mejora del css

---
+++ Tarjetas/codigo.js
@@ -0,0 +1,1 @@
+const volver = document.getElementById("opc")
---
+++ codigo.js
@@ -0,0 +1,5 @@
+window.addEventListener('loop', () =>{
+    const contenedor_loader = document.querySelector('.CenterBike')
+    contenedor_loader.style.opacity = 0
+    contenedor_loader.style.visibility = 'hidden'
+});
