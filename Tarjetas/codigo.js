a9857543d70aaf529530a2ff0018e101f0858b81
Leonardo Cardozo <leonardocardozo153@gmail.com>
2023-04-16 10:56:32 -0300

    nueva pagina

--- Tarjetas/codigo.js
+++
@@ -1,1 +0,0 @@
-const volver = document.getElementById("opc")
--- codigo.js
+++ codigo.js
@@ -1,5 +1,4 @@
-window.addEventListener('loop', () =>{
-    const contenedor_loader = document.querySelector('.CenterBike')
-    contenedor_loader.style.opacity = 0
-    contenedor_loader.style.visibility = 'hidden'
-});
+function redirectToPage(){
+    window.location.href = "../index.html";
+
+}
