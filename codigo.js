e12ec1ea6403cdb3c484af8803f515a9b2f5c61e
Leonardo Cardozo <leonardocardozo153@gmail.com>
2023-04-17 09:27:33 -0300

    mejora del css

--- codigo.js
+++ codigo.js
@@ -1,4 +1,33 @@
 function redirectToPage(){
     window.location.href = "../index.html";
-
+}
+function redirectToPage1(){
+	window.location.href = "Tarjetas/Tarjeta_1.html"
+}
+function redirectToPage2(){
+	window.location.href = "Tarjetas/Tarjeta_2.html"
+}
+function redirectToPage3(){
+	window.location.href = "Tarjetas/Tarjeta_3.html"
+}
+function redirectToPage4(){
+	window.location.href = "Tarjetas/Tarjeta_4.html"
+}
+function redirectToPage5(){
+	window.location.href = "Tarjetas/Tarjeta_5.html"
+}
+function redirectToPage6(){
+	window.location.href = "Tarjetas/Tarjeta_6.html"
+}
+function redirectToPage7(){
+	window.location.href = "Tarjetas/Tarjeta_7.html"
+}
+function redirectToPage8(){
+	window.location.href = "Tarjetas/Tarjeta_8.html"
+}
+function redirectToPage9(){
+	window.location.href = "Tarjetas/Tarjeta_9.html"
+}
+function redirectToPage10(){
+	window.location.href = "Tarjetas/Tarjeta_10.html"
 }
