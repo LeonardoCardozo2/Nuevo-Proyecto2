3e78a5a6722280c55fcf38e9e87a551ef3d8648b
Leonardo Cardozo <leonardocardozo153@gmail.com>
2023-04-26 14:42:22 -0300

    v

--- codigo.js
+++ codigo.js
@@ -13,21 +13,3 @@ function redirectToPage3(){
 function redirectToPage4(){
 	window.location.href = "Tarjetas/Tarjeta_4.html"
 }
-function redirectToPage5(){
-	window.location.href = "Tarjetas/Tarjeta_5.html"
-}
-function redirectToPage6(){
-	window.location.href = "Tarjetas/Tarjeta_6.html"
-}
-function redirectToPage7(){
-	window.location.href = "Tarjetas/Tarjeta_7.html"
-}
-function redirectToPage8(){
-	window.location.href = "Tarjetas/Tarjeta_8.html"
-}
-function redirectToPage9(){
-	window.location.href = "Tarjetas/Tarjeta_9.html"
-}
-function redirectToPage10(){
-	window.location.href = "Tarjetas/Tarjeta_10.html"
-}
